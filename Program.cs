391709cc7edc1a9fc8757481387789121496d46d
ayhantefik <ayhantefik93@gmail.com>
2022-11-28 13:48:03 +0100

    MainMenu1

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
 using System.Runtime.InteropServices;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Snake
 {
     internal class Program
     {
-        public static Snake[,] board = new Snake[20, 30];
+        public static Snake[,] board = new Snake[50, 50];
         public static int direction = 0; //0=north, 1=east, 2=south, 3=west
         public static int score = 0;
         public static int highScore = 0;
-        public static string playerName="";
+        public static string playerName = "";
         public static int snakeSize = 0;
+        public static ConsoleKeyInfo keyPress = new ConsoleKeyInfo();
 
         public enum Snake
         {
@@ -21,66 +28,92 @@ namespace Snake
         }
         public static void ResetBoard()
         {
-            snakeSize= 0;
+            snakeSize = 0;
             score = 0;
             direction = 0;
+            ///loop to reset the board to starting position
+            for (int i = 0; i < 51; i++)
             {
-                for(int i = 0; i < board.GetLength(0); i++)
+                for (var j = 0; j < 51; j++)
                 {
-                    for (var j = 0; j < board.GetLength(1); j++)
-                    {
-                        if (i == 0 || i == board.GetLength(0) - 1 || j == 0 || j == board.GetLength(1) - 1) board[i, j] = Snake.Wall;
-                        else if (i == 5 && j == 5) board[i, j] = Snake.Head;
-                        else if (i == 10 && j == 10) board[i, j] = Snake.Fruit;
-                        else board[i, j] = Snake.Empty;
-                    }
+                    if (i == 0 || i == 50 || j == 0 || j == 50) board[i, j] = Snake.Wall;
+                    else if (i == 25 && j == 25) board[i, j] = Snake.Head;
+                    else board[i, j] = Snake.Empty;
                 }
-                
             }
         }
-        public static void DrawBoard()
+        static void StartMenu()
         {
-            Console.WriteLine($"Player name: {playerName}");
-            Console.WriteLine($"score: {score}");
-            for (int i = 0; i < board.GetLength(0); i++)
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("--------------Welcome to snake---------------");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to play");
+            Console.WriteLine();
+            Console.WriteLine("Press L to high score list");
+            Console.WriteLine("Press H to help");
+            Console.WriteLine("Press Esc to quit");
+
+            keyPress = Console.ReadKey(true);
+            if (keyPress.Key == ConsoleKey.L)
             {
-                for (int j = 0; j < board.GetLength(1); j++)
-                {
-                    if(board [i, j] == Snake.Empty)
-                    {
-                        Console.Write(' ');
-                    }
-                    if (board[i, j] == Snake.Wall)
-                    {
-                        Console.Write("@", Console.ForegroundColor = ConsoleColor.Magenta);
-                    }
-                    if (board[i, j] == Snake.Head)
-                    {
-                        Console.Write("O", Console.ForegroundColor = ConsoleColor.Green);
-                    }
-                    if (board[i, j] == Snake.Body)
-                    {
-                        Console.Write("o", Console.ForegroundColor = ConsoleColor.DarkGreen);
-                    }
-                    if (board[i, j] == Snake.Fruit)
-                    {
-                        Console.Write("*", Console.ForegroundColor = ConsoleColor.Red);
-                    }
-                }
-                Console.WriteLine();
+                HighScore();
+            }
+            if (keyPress.Key == ConsoleKey.H)
+            {
+                Help();
+            }
+            if (keyPress.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+            Console.WriteLine();
+        }
+        static void HighScore()
+        {
+            Console.WriteLine();
+            Console.WriteLine("High Score List:");
+            Console.WriteLine();
+
+            Console.WriteLine("Press B for back to main manu");
+            Console.WriteLine("Press Esc to quit");
+            keyPress = Console.ReadKey(true);
+            if (keyPress.Key == ConsoleKey.B)
+            {
+                StartMenu();
+            }
+            if (keyPress.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
             }
         }
-
+        static void Help()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hepl Page:");
+            Console.WriteLine();
+            Console.WriteLine("Press B for back to main manu");
+            Console.WriteLine("Press Esc to quit");
+            keyPress = Console.ReadKey(true);
+            if (keyPress.Key == ConsoleKey.B)
+            {
+                StartMenu();
+            }
+            if (keyPress.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+        }
         static void Main(string[] args)
         {
+            StartMenu();
             ResetBoard();
-            DrawBoard();
-            string choice="";
-
+            string choice = "";
             do
             {
 
-            } while (choice!="quit");
+            } while (choice != "quit");
         }
     }
 }
