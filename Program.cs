881aece481fd74476dc5a976953a98bc4bd9baac
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2022-11-28 13:02:55 +0100

    Klar

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿namespace Snake
+﻿using System.Runtime.InteropServices;
+
+namespace Snake
 {
     internal class Program
     {
-        public static Snake[,] board = new Snake[50, 50];
+        public static Snake[,] board = new Snake[20, 30];
         public static int direction = 0; //0=north, 1=east, 2=south, 3=west
         public static int score = 0;
         public static int highScore = 0;
         public static string playerName="";
+        
         public enum Snake
         {
             Empty,       // 0
@@ -19,19 +22,55 @@
         {
             score = 0;
             direction = 0;
-            for(int i = 0; i < 51; i++)
+            for(int i = 0; i < board.GetLength(0); i++)
             {
-                for(var j = 0; j < 51; j++)
+                for(var j = 0; j < board.GetLength(1); j++)
                 {
-                    if (i == 0 || i == 50 || j==0 || j ==50) board[i, j] = Snake.Wall;
-                    else if (i==25 && j==25) board[i, j] = Snake.Head;
+                    if (i == 0 || i == board.GetLength(0)-1 || j==0|| j == board.GetLength(1)-1) board[i, j] = Snake.Wall;
+                    else if (i==5 && j==5) board[i, j] = Snake.Head;
+                    else if(i==10 && j==10) board[i, j] = Snake.Fruit;
                     else board[i, j] = Snake.Empty;                    
                 }
             }
         }
+        public static void DrawBoard()
+        {
+            Console.WriteLine($"Player name: {playerName}");
+            Console.WriteLine($"score: {score}");
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if(board [i, j] == Snake.Empty)
+                    {
+                        Console.Write(' ');
+                    }
+                    if (board[i, j] == Snake.Wall)
+                    {
+                        Console.Write("@", Console.ForegroundColor = ConsoleColor.Magenta);
+                    }
+                    if (board[i, j] == Snake.Head)
+                    {
+                        Console.Write("O", Console.ForegroundColor = ConsoleColor.Green);
+                    }
+                    if (board[i, j] == Snake.Body)
+                    {
+                        Console.Write("o", Console.ForegroundColor = ConsoleColor.DarkGreen);
+                    }
+                    if (board[i, j] == Snake.Fruit)
+                    {
+                        Console.Write("*", Console.ForegroundColor = ConsoleColor.Red);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
         static void Main(string[] args)
         {
-            string choice="";
+            ResetBoard();
+            DrawBoard();
+        string choice="";
             do
             {
 
